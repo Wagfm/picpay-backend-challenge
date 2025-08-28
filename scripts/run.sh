1eb85fd8007fd93d92729d6f733753ccf32f77d1
Wagner Maciel <wagner.varginha@hotmail.com>
2025-08-27 22:52:24 -0300

    initial commit

---
+++ scripts/run.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+ROOT_FOLDER="backend-challenge-picpay"
+SCRIPTS_FOLDER="scripts"
+ENVIRONMENT=$1
+ENVIRONMENT=${ENVIRONMENT:='dev'}
+
+function fatal(){
+    echo "$1"
+    exit 3;
+}
+
+function lower() {
+    printf '%s\n' "${1,,}"
+}
+
+function basename() {
+    local tmp
+    tmp=${1%"${1##*[!/]}"}
+    tmp=${tmp##*/}
+    tmp=${tmp%"${2/"$tmp"}"}
+    printf '%s\n' "${tmp:-/}"
+}
+
+function setup_dev(){
+    return 0;
+}
+
+CURRENT_DIR="$(basename "$(pwd)")"
+
+if [ "$CURRENT_DIR" != "$ROOT_FOLDER" ]; then
+    if [ "$CURRENT_DIR" != "$SCRIPTS_FOLDER" ]; then
+        fatal 'Invalid execution folder!'
+    fi
+    cd ..
+fi
+
+case "$(lower "$ENVIRONMENT")" in
+    "dev")
+        cd docker || fatal "Folder 'docker' not found!";
+            docker compose -f docker-compose.dev.yml down --remove-orphans
+            docker compose -f docker-compose.dev.yml up -d --build
+        cd ..
+    ;;
+    "prod")
+        echo 'not implemented...'
+    ;;
+    *)
+        fatal "Invalid environment specification!"
+    ;;
+esac
